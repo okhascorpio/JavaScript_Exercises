8fb059ad790e11756a3a99296b8d9ac80ab998d6
okhascorpio <okhascorpio@gmail.com>
2022-12-04 12:46:05 +0200

    js arrays

---
+++ js_ex_17_array.js
@@ -0,0 +1,16 @@
+/*
+Common practice to create js array with 'const' key word
+For simplicity, readability and execution speed, 
+    use the array literal method, not the 'new Array()' method.
+Access or assign array elements by index e.g. 'cars[0]' is the first element.
+Access full array by the array name.
+*/
+
+
+const cars = [
+  "Saab",
+  "Volvo",
+  "BMW"
+]
+document.getElementById("demo").innerHTML = cars;
+console.log("Is 'cars' an array? "+Array.isArray(cars))
